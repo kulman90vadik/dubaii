d4b47e4bc7d6bd68baec31609dc17abe91ddec21
Vadim Kulman <vadim.kulman90@mail.ru>
2021-12-12 16:52:24 +0300

    add catalogs

--- app/js/main.js
+++ app/js/main.js
@@ -30,7 +30,63 @@ window.addEventListener("load", function(){
         document.querySelector('.popup').classList.toggle('popup--active');
     });
 
+    let reservationBtnNumber = document.querySelector('.reservation__btn--number');
+    let reservationBtnBottom =  document.querySelector('.reservation__btn--bottom');
+
+// 1 
+    // reservationBtnNumber.addEventListener('click', function(){
+    //     reservationBtnBottom.classList.toggle('reservation__btn--mann');
+    // });
+// 2
+    // reservationBtnBottom.addEventListener('click', function(){
+    //     reservationBtnBottom.value = reservationBtnNumber.value;
+    //     reservationBtnBottom.querySelector('.reservation__text').innerHTML = reservationBtnNumber.value;
+    //     reservationBtnBottom.classList.toggle('reservation__btn--mann');
+    //     reservationBtnNumber.value = reservationBtnBottom.value;
+    //     reservationBtnNumber.querySelector('.reservation__text').innerHTML = reservationBtnBottom.value;
+    // });
+
+//  ВЫПАДАЮЩИЙ СПИСОК - ВЫБОР ГОРОДА
+    document.querySelector('.reservation__btn--select').addEventListener('click', function(){
+        document.querySelector('.reservation__select').classList.toggle('reservation__select--active');
+    });
+    document.querySelectorAll('.reservation__items').forEach(item => {
+        item.addEventListener('click', function(){
+            let valueItem = item.textContent;
+            document.querySelector('.reservation__btn--select').innerHTML = valueItem;
+            document.querySelector('.reservation__btn--select').value = valueItem;
+            document.querySelector('.reservation__select').classList.toggle('reservation__select--active');
+        });
+    });
+    
+// КАТАЛОГ ФИЛЬТР -- SELECT
+
+    let sidebarButtons = document.querySelectorAll('.sidebar__button');
+    sidebarButtons.forEach(btn => {
+        btn.addEventListener('click', function(){
+            let sidebarFilter = btn.closest('.sidebar__filter');
+            sidebarFilter.querySelector('.sidebar__list').classList.toggle('sidebar__list--active');
+        });
+    });
 
 
-
+//  РАЗВЕРНУТЬ/СКРЫИТ catalogs-card
+    let catalogsDots = document.querySelector('.catalogs-card__text--dots');
+    let catalogsMore = document.querySelector('.catalogs-card__text--more');
+    let catalogsBtnSpan = document.querySelector('.catalogs-card__add span')
+    document.querySelector('.catalogs-card__add').addEventListener('click', function(){
+        document.querySelector('.catalogs-card__icon').classList.toggle('catalogs-card__icon--active');
+        if(catalogsDots.style.display === 'none') {
+            catalogsDots.style.display = 'inline';
+            catalogsBtnSpan.innerHTML = 'Развернуть';
+            catalogsMore.style.display = 'none';
+        }
+        else {
+            catalogsDots.style.display = 'none';
+            catalogsBtnSpan.innerHTML = 'Скрыть';
+            catalogsMore.style.display = 'inline';
+        }
+    });
+    // СДЕЛАТЬ МНОЖЕСТВЕННЫЙ ВЫБОР КАРД
+        
 });
